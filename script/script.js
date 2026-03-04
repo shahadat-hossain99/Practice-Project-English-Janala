c13ec94f3ca74a779b37f072bd7e7bae404d0111
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-03-04 10:53:48 +0600

    lesson word card completed + default ui done + clicked btn active

--- script/script.js
+++ script/script.js
@@ -8,28 +8,80 @@ const loadLessons = () => {
     .then((json) => displayLessons(json.data));
 };
 
+const removeActive = () => {
+  const lessonButtons = document.querySelectorAll(".lesson-btn");
+  lessonButtons.forEach((btn) => btn.classList.remove("active"));
+};
+
 const loadLevelWord = (id) => {
   console.log(id);
   const url = ` https://openapi.programming-hero.com/api/level/${id}`;
   console.log(url);
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayLevelWord(data.data));
+    .then((data) => {
+      removeActive(); //remove all active class
+      const clickedBtn = document.getElementById(`lesson-btn-${id}`);
+      console.log(clickedBtn);
+      clickedBtn.classList.add("active"); // add active class
+
+      displayLevelWord(data.data);
+    });
 };
 
 const displayLevelWord = (levelWords) => {
   console.log(levelWords);
 
   const wordContainer = document.getElementById("word-container");
-  // wordContainer.innerHTML = "";
+  wordContainer.innerHTML = "";
+
+  if (levelWords == 0) {
+    wordContainer.innerHTML = `
+     
+     <div class="text-center col-span-full space-y-4 py-10">
+         <img src="./assets/alert-error.png" alt="" class="mx-auto" />
+        <p class="font-normal text-base font-bangla">
+          এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+        </p>
+        <h3 class="font-medium text-4xl font-bangla">
+          নেক্সট Lesson এ যান
+        </h3>
+      </div>
+
+     `;
+
+    return;
+  }
 
   levelWords.forEach((word) => {
     const wordCard = document.createElement("div");
 
+    // {
+    //     "id": 5,
+    //     "level": 1,
+    //     "word": "Eager",
+    //     "meaning": "আগ্রহী",
+    //     "pronunciation": "ইগার"
+    // }
+
     wordCard.innerHTML = `
-    
+
+     <div class="bg-white rounded shadow-sm text-center py-12 px-5 space-y-6">
+        <h3 class="text-3xl font-bold">${word.word ? word.word : "তথ্য পাওয়া যায়নি"}</h3>
+        <p class="text-sm font-medium">Meaning /Pronounciation</p>
+        <div class="text-3xl font-semibold font-bangla">${word.meaning ? word.meaning : "তথ্য পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "তথ্য পাওয়া যায়নি"}</div>
+        <div class="flex justify-between items-center">
+          <button class="btn bg-cyan-50 hover:bg-cyan-100">
+            <i class="fa-solid fa-circle-info"></i>
+          </button>
+          <button class="btn bg-cyan-50 hover:bg-cyan-100">
+            <i class="fa-solid fa-volume-high"></i>
+          </button>
+        </div>
+      </div>
     
     `;
+    wordContainer.appendChild(wordCard);
   });
 };
 
@@ -45,7 +97,7 @@ const displayLessons = (lessons) => {
     const lessonCard = document.createElement("div");
     lessonCard.innerHTML = `
 
-     <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary font-semibold text-sm">
+     <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary font-semibold text-sm lesson-btn">
         <i class="fa-solid fa-book-open"></i> Lesson ${lesson.level_no}
         </button>
     
